f07f61c4ca301322c557472b08c8c1b989c56b55
almat <almat_kenen@mail.ru>
2017-08-25 19:31:32 +0600

    instructor used added

--- server/fabfile/install.py
+++ server/fabfile/install.py
@@ -22,6 +22,7 @@ def install():
     sudo("curl -L https://github.com/docker/compose/releases/download/1.10.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose")
     sudo("chmod +x /usr/local/bin/docker-compose")
 
+
 @task
 def git_install():
     """
