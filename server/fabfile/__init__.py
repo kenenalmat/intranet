bdd19368bfe6a01f1157f6c617d928bc13811cdd
almat <almat_kenen@mail.ru>
2017-08-25 19:35:48 +0600

    instructor used added

--- server/fabfile/__init__.py
+++ server/fabfile/__init__.py
@@ -7,6 +7,6 @@ import install
 
 env.repository = "https://github.com/applet97/intranet.git"
 env.user = "ubuntu"
-env.hosts = ["52.57.202.178"]
+env.hosts = ["35.156.208.108"]
 env.key_filename = "~/intranet.pem"
 env.filename = "docker-compose.yml"
